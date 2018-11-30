3ec1700ba0d7dcdd983c58586eb44f643cdc6567
oleblaesing <ole.blaesing@web.de>
2018-11-30 18:39:10 +0100

    Deliver as a PWA

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', function (e) {
+  e.waitUntil(caches.open('myonsenuipwa').then(cache => cache.addAll([
+    '/norsk/index.html',
+    '/norsk/manifest.json',
+  ])))
+})
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)))
+})
